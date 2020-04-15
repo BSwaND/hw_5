b548226e71c2210df4c35bedf73c6559f974cd61
Denis <d4717549@gmail.com>
2020-04-15 22:30:00 +0300

    stabl version 1.21

--- src/Myclass/Myclass.php
+++ src/Myclass/Myclass.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace src\Myclass;
+namespace app\Myclass;
 
 
 class Myclass
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -13,11 +13,7 @@ final class Autoload
                 $keyPrefix = key($this->prefix);
                 $class = str_replace($keyPrefix, $this->prefix[$keyPrefix], $class);
 
-
-
-                //var_dump($this->prefix[$keyPrefix] . '/'. str_replace('\\', '/', $class) . '.php');
-                return require_once  str_replace('\\', '/', $class) . '.php';
-                //return require_once  ('src/Myclass/Myclass.php');
+                require_once  str_replace('\\', '/', $class) . '.php';
             }
         });
     }
